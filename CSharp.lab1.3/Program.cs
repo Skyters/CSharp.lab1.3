5636ebc62d9273cfebf89fdedea691ec5ef238db
Utka <vlad-307123@mail.ru>
2026-03-04 09:54:31 +0800

    Добавьте файлы проекта.

---
+++ CSharp.lab1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp.lab1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
