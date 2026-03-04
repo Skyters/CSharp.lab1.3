f08bc2ada5bb72012437f9e5293e730499140176
Utka <vlad-307123@mail.ru>
2026-03-04 09:56:49 +0800

    Был написан код для выполнения задания.

--- CSharp.lab1.3/Program.cs
+++ CSharp.lab1.3/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите предложение: ");
+            string str = Console.ReadLine();
+
+            int count = 0;
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]))
+                {
+                    count++;
+                }
+
+            }
+
+            double percent = (double)count / str.Length * 100;
+            result += "\nБукв: " + count;
+            result += "\nПроцент букв: " + percent.ToString("F2");
+
+            Console.WriteLine(result);
         }
     }
 }
