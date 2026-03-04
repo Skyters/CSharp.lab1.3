a6380ecab55e68c555c8ed57a0119772ce3350c0
Utka <vlad-307123@mail.ru>
2026-03-04 10:03:22 +0800

    Были сделаны тесты.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using CSharp.lab1._3;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var result = Logic.Result("Буква");
+            Assert.That(result.Contains("Букв: 5"));
+            Assert.That(result.Contains("Процент букв: 100,00"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = Logic.Result("Буква 123");
+            Assert.That(result.Contains("Букв: 5"));
+            Assert.That(result.Contains("Процент букв: 55,56"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var result = Logic.Result("123");
+            Assert.That(result.Contains("Букв: 0"));
+            Assert.That(result.Contains("Процент букв: 0,00"));
+        }
+    }
+}
